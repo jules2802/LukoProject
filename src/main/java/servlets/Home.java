45f44bb197e8253604ef4bfd8327aef3e368cc41
jules2802 <jules-antho@live.fr>
2019-03-06 20:33:54 +0100

    First commit

---
+++ src/main/java/servlets/Home.java
@@ -0,0 +1,136 @@
+package servlets;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+
+@WebServlet("/accueil")
+public class Home extends  CreateTemplateEngine {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+
+        //First page launch
+        WebContext context = new WebContext(req,resp, req.getServletContext());
+        TemplateEngine engine = CreateTemplateEngine(req, resp);
+        engine.process("Home", context, resp.getWriter());
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+
+        //TODO rajouter un try and catch qui prends tout les requetes
+        //Get data
+        String longitude= req.getParameter("longitude");
+        String latitude= req.getParameter("latitude");
+
+        String urlINDEE="http://www.georisques.gouv.fr/webappReport/ws/insee/commune/_search?lon="+longitude+"&lat="
+        +latitude;
+
+        URL obj = new URL(urlINDEE);
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+        // optional default is GET
+        con.setRequestMethod("GET");
+        con.setRequestProperty("User-Agent", "CHROME");
+        int responseCode = con.getResponseCode();
+        System.out.println("\nSending 'GET' request to URL : " + urlINDEE);
+        System.out.println("Response Code : " + responseCode);
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuffer response = new StringBuffer();
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
+        //print in String
+        System.out.println(response.toString());
+        String codeInsee="",commune="";
+
+        JSONParser parse = new JSONParser();
+
+       try{
+           //Read JSON response and and get codeInse and place name
+            Object json = parse.parse(response.toString());
+            JSONObject jsonObject = (JSONObject) json;
+            codeInsee= (String) jsonObject.get("codeInsee");
+            commune= (String) jsonObject.get("libelleCommune");
+            commune=commune.toLowerCase();
+
+
+        }catch(Exception ex){
+            ex.printStackTrace();
+           }
+
+
+        //send request to get risks
+        String urlRisks="http://www.georisques.gouv.fr/webappReport/ws/gasparv2/risqueDetail/"+codeInsee;
+        URL risks = new URL(urlRisks);
+        HttpURLConnection riskConnection = (HttpURLConnection) risks.openConnection();
+        // optional default is GET
+        riskConnection.setRequestMethod("GET");
+        riskConnection.setRequestProperty("User-Agent", "CHROME");
+        int responseCodeRisks = riskConnection.getResponseCode();
+
+        System.out.println("\nSending 'GET' request to URL : " + urlRisks);
+        System.out.println("Response Code : " + responseCodeRisks);
+
+        BufferedReader risksBuffer = new BufferedReader(
+                new InputStreamReader(riskConnection.getInputStream()));
+        String inputLineRisks;
+
+        StringBuffer responseRisks = new StringBuffer();
+        while ((inputLineRisks = risksBuffer.readLine()) != null) {
+            responseRisks.append(inputLineRisks);
+        }
+        risksBuffer.close();
+        //print in String
+        System.out.println(responseRisks.toString());
+
+        //List of risks
+        ArrayList<String> risksList= new ArrayList<>();
+    try{
+        //Read JSON response and and get codeInsee
+        Object jsonRisks = parse.parse(responseRisks.toString());
+        JSONArray jsonRisksObject = (JSONArray) jsonRisks;
+
+        for (Object a : jsonRisksObject) {
+            if (a instanceof JSONObject) {
+                Object element = ((JSONObject) a).get("libRisqueLong");
+                risksList.add(element.toString());
+
+                System.out.println(element.toString());
+                System.out.println(element.toString().getBytes(StandardCharsets.UTF_8));
+            }
+        }
+
+
+    }catch(Exception ex){
+        ex.printStackTrace();
+    }
+
+    //Send parameters in session
+        req.getSession().setAttribute("longitude",longitude);
+        req.getSession().setAttribute("latitude",latitude);
+        req.getSession().setAttribute("codeInsee",codeInsee);
+        req.getSession().setAttribute("commune",commune);
+        req.getSession().setAttribute("risksList",risksList);
+        resp.sendRedirect("/results");
+
+}
+
+}
---
+++ src/main/java/servlets/ResultsServlet.java
@@ -0,0 +1,147 @@
+package servlets;
+
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+import org.apache.commons.lang3.StringEscapeUtils;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import static org.unbescape.html.HtmlEscape.escapeHtml5;
+import static org.unbescape.html.HtmlEscape.escapeHtml5Xml;
+import static sun.text.normalizer.Utility.escape;
+
+@WebServlet("/results")
+public class ResultsServlet extends CreateTemplateEngine {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+
+        //Launch page
+        WebContext context = new WebContext(req, resp, req.getServletContext());
+
+        //List with accents
+        ArrayList<String> listRisks = (ArrayList<String>) req.getSession().getAttribute("risksList");
+
+        //List depends of risk type
+        ArrayList<String> floodRisks = new ArrayList<>();
+        ArrayList<String> industrialRisks = new ArrayList<>();
+        ArrayList<String> earthRisks = new ArrayList<>();
+        ArrayList<String> pollutionRisks = new ArrayList<>();
+
+        //how risky is the risk
+        ArrayList<String> floodRisksNumber = new ArrayList<>();
+        ArrayList<String> industrialRisksNumber = new ArrayList<>();
+        ArrayList<String> earthRisksNumber = new ArrayList<>();
+        ArrayList<String> pollutionRisksNumber = new ArrayList<>();
+
+        //total risk
+        int floodTotal=0;
+        int earthTotal=0;
+        int industrialTotal=0;
+        int pollutionTotal=0;
+
+
+        for (int i = 0; i < listRisks.size(); i++) {
+            String risk = listRisks.get(i);
+
+
+            if (risk.contains("Inondation") || risk.contains("météorologiques")) {
+                //add risk to good category
+                floodRisks.add(escapeHtml5(risk));
+
+                //estimate how risky
+                if (risk.contains("Inondation")){
+                    floodRisksNumber.add("8");
+                    floodTotal+= 8;
+                }else{
+                    floodRisksNumber.add("5");
+                    floodTotal+= 5;
+                }
+
+
+            } else if (risk.contains("industriel") || risk.contains("industriels") || risk.contains("marchandises")) {
+                industrialRisks.add(escapeHtml5(risk));
+
+                if(risk.contains("industriel") || risk.contains("industriels")){
+                    industrialRisksNumber.add("3");
+                    industrialTotal+= 3;
+                }else{
+                    industrialRisksNumber.add("1");
+                    industrialTotal+= 1;
+                }
+
+            } else if (risk.contains("Séisme") || risk.contains("terrain") || risk.contains("terrains") || risk.contains("Mouvement")) {
+                earthRisks.add(escapeHtml5(risk));
+
+                if(risk.contains("Séisme")){
+                    earthRisksNumber.add("6");
+                    earthTotal+= 6;
+                }else{
+                    earthRisksNumber.add("7");
+                    earthTotal+= 7;
+                }
+
+            } else if (risk.contains("radon") || risk.contains("pollution")) {
+                pollutionRisks.add(escapeHtml5(risk));
+
+                if(risk.contains("radon")){
+                    pollutionRisksNumber.add("4");
+                    pollutionTotal+= 4;
+                }else{
+                    pollutionRisksNumber.add("5");
+                    pollutionTotal+= 5;
+                }
+            }
+
+        }
+           if(floodRisks.size()==0 || floodRisksNumber.size()==0){
+                    floodRisks.add("Lucky you! Ton adresse n'a pas de risques d'innondations");
+                    floodRisksNumber.add("0");}
+           if( pollutionRisks.size()==0 || pollutionRisksNumber.size()==0){
+                    pollutionRisks.add("Lucky you! Ton adresse n'a pas de risques de pollutions");
+                    pollutionRisksNumber.add("0");}
+           if(earthRisks.size()==0 || earthRisksNumber.size()==0){
+                    earthRisks.add("Lucky you! Ton adresse n'a pas de risques de séismes ou mouvements de terrains");
+                    earthRisksNumber.add("0");}
+           if (industrialRisks.size()==0 || industrialRisksNumber.size()==0){
+                    industrialRisks.add("Lucky you! Ton adresse n'a pas de risques industriels");
+                    industrialRisksNumber.add("0");
+           }
+
+
+
+        //set variable in context to get it in html File
+        context.setVariable("floodRisks", floodRisks);
+        context.setVariable("earthRisks", earthRisks);
+        context.setVariable("pollutionRisks", pollutionRisks);
+        context.setVariable("industrialRisks", industrialRisks);
+
+        context.setVariable("floodRisksNumber",floodRisksNumber);
+        context.setVariable("earthRisksNumber",earthRisksNumber);
+        context.setVariable("industrialRisksNumber",industrialRisksNumber);
+        context.setVariable("pollutionRisksNumber",pollutionRisksNumber);
+
+        context.setVariable("floodTotal",floodTotal);
+        context.setVariable("earthTotal",earthTotal);
+        context.setVariable("industrialTotal",industrialTotal);
+        context.setVariable("pollutionTotal",pollutionTotal);
+
+        context.setVariable("longitude",req.getSession().getAttribute("longitude"));
+        context.setVariable("latitude",req.getSession().getAttribute("latitude"));
+        context.setVariable("codeInsee", req.getSession().getAttribute("codeInsee"));
+        context.setVariable("commune", req.getSession().getAttribute("commune"));
+
+
+        TemplateEngine engine = CreateTemplateEngine(req, resp);
+        engine.process("Results", context, resp.getWriter());
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) {
+    }
+}
